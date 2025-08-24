8979f4d16c59298cc56564cdb9d142cb9e545e15
arCt1cX <92270123+arCt1cX@users.noreply.github.com>
2025-08-24 03:29:47 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,6 @@ import { LibraryProvider } from './contexts/LibraryContext';
 import { JapaneseProvider } from './contexts/JapaneseContext';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Library from './pages/Library';
-import Reader from './pages/Reader';
-import Search from './pages/Search';
-import Settings from './pages/Settings';
 
 function App() {
   return (
@@ -19,10 +15,6 @@ function App() {
             <main className="pb-20">
               <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/library" element={<Library />} />
-                <Route path="/search/:site?" element={<Search />} />
-                <Route path="/reader/:site/:mangaId/:chapterId?" element={<Reader />} />
-                <Route path="/settings" element={<Settings />} />
               </Routes>
             </main>
           </div>
