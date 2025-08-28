0c24ae9b42d99574e53e970780dc687116d1d963
arCt1cX <92270123+arCt1cX@users.noreply.github.com>
2025-08-29 01:03:02 +0200

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -26,7 +26,7 @@ export const DEFAULT_MANGA_SITES = [
     url: 'https://mangaworld.cx',
     color: '#ff6740',
     description: 'Italian manga site with latest releases',
-    logo: '/mangaworld-favicon-32x32.png'
+  logo: '/MangaReader-jp/mangaworld-favicon-32x32.png'
   },
   {
     id: 'manga-italia',
@@ -34,7 +34,7 @@ export const DEFAULT_MANGA_SITES = [
     url: 'https://www.manga-italia.com',
     color: '#009639',
     description: 'Italian manga community site',
-    logo: '/manga-italia-favicon.png'
+  logo: '/MangaReader-jp/manga-italia-favicon.png'
   }
 ];
 
